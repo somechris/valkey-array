0e15c72de96ee13b8d77b51d8721f55b06912bbb
Christian Aistleitner <christian@quelltextlich.at>
2026-09-10 10:04:55 +0200

    array: Add unit tests

--- src/array.rs
+++ src/array.rs
@@ -70,3 +70,117 @@ impl Array {
         self.values.iter()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    mod util {
+        use valkey_module::ValkeyString;
+        use crate::Array;
+
+        pub fn vkstr<S: Into<String>>(input: S) -> ValkeyString {
+            ValkeyString::test(input.into())
+        }
+
+        pub fn assert_array_entry<S: Into<String>>(array: &Array, position: u64, expected: S) {
+            let value =array.get(&position).unwrap_or_else(|| panic!("array should have a value at {position}"));
+
+            let expected_str = vkstr(expected);
+            assert_eq!(*value, expected_str, "\"{}\" == \"{}\"", value.to_string(), expected_str);
+        }
+
+        pub fn assert_array_no_entry(array: &Array, position: u64) {
+            if let Some(entry) = array.get(&position) {
+                panic!("array should be empty but is \"{}\" at {position}", entry.to_string());
+            }
+        }
+    }
+    use crate::Array;
+    use crate::array::tests::util::{assert_array_entry, assert_array_no_entry, vkstr};
+
+    #[test]
+    fn array_basic_get_set() {
+        let mut array = Array::new();
+
+        // No entries in the empty array
+        assert_array_no_entry(&array, 23);
+        assert_array_no_entry(&array, 42);
+        assert_array_no_entry(&array, 4711);
+
+        // Adding a single slot
+        let added_slots = array.set(&23, &vkstr("foo"));
+        assert_eq!(added_slots, 1); // New slot, as it was empty before
+
+        assert_array_entry(&array, 23, "foo");
+        assert_array_no_entry(&array, 42);
+        assert_array_no_entry(&array, 4711);
+
+        // Adding a different slot
+        let added_slots = array.set(&42, &vkstr("bar"));
+        assert_eq!(added_slots, 1);
+
+        assert_array_entry(&array, 23, "foo");
+        assert_array_entry(&array, 42, "bar");
+        assert_array_no_entry(&array, 4711);
+
+        // Adding to first slot again
+        let added_slots = array.set(&23, &vkstr("baz"));
+        assert_eq!(added_slots, 0); // No new slot, as it was occupied before
+
+        assert_array_entry(&array, 23, "baz");
+        assert_array_entry(&array, 42, "bar");
+        assert_array_no_entry(&array, 4711);
+    }
+
+    #[test]
+    fn array_del() {
+        let mut array = Array::new();
+
+        // Deleting an unused slot
+        assert!(array.del(&42).is_none());
+
+        // Setting a slot and deleting it again
+        array.set(&42, &vkstr("bar"));
+        let deleted = array.del(&42).expect("del should yield the old value");
+        assert_eq!(deleted, vkstr("bar"));
+    }
+
+    #[test]
+    fn array_count() {
+        let mut array = Array::new();
+
+        assert_eq!(array.count(), 0);
+
+        array.set(&42, &vkstr("bar"));
+        assert_eq!(array.count(), 1);
+
+        array.set(&23, &vkstr("baz"));
+        assert_eq!(array.count(), 2);
+    }
+
+    #[test]
+    fn array_next_highest_position() {
+        let mut array = Array::new();
+
+        assert_eq!(array.next_highest_position(), 0);
+
+        array.set(&42, &vkstr("bar"));
+        assert_eq!(array.next_highest_position(), 43);
+
+        array.set(&23, &vkstr("baz"));
+        assert_eq!(array.next_highest_position(), 43);
+    }
+
+    #[test]
+    fn array_next_highest_position_after_deletion() {
+        let mut array = Array::new();
+
+        array.set(&23, &vkstr("baz"));
+        array.set(&42, &vkstr("bar"));
+
+        array.del(&42).expect("del should yield the old value");
+        assert_eq!(array.next_highest_position(), 24);
+
+        array.del(&23).expect("del should yield the old value");
+        assert_eq!(array.next_highest_position(), 0);
+    }
+}
