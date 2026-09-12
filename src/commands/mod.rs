9a15182b853acebd6594eefd08d08a3b3f41b3a7
Christian Aistleitner <christian@quelltextlich.at>
2026-09-12 19:53:47 +0200

    commands: Dedupe command importing

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,16 +1,18 @@
 //! Implementations of array commands
 
-pub mod arcount;
-pub mod ardel;
-pub mod arget;
-pub mod arinsert;
-pub mod arlen;
-pub mod arset;
 pub mod utils;
 
-pub use arcount::arcount;
-pub use ardel::ardel;
-pub use arget::arget;
-pub use arinsert::arinsert;
-pub use arlen::arlen;
-pub use arset::arset;
+/// Import a command's module and publishes the command
+macro_rules! add_command {
+    ($name:ident) => {
+        pub mod $name;
+        pub use $name::$name;
+    };
+}
+
+add_command!(arcount);
+add_command!(ardel);
+add_command!(arget);
+add_command!(arinsert);
+add_command!(arlen);
+add_command!(arset);
